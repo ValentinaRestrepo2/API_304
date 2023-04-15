6cbca24928a7e96ff772b87bd59f1db5d7bfe80c
Valentina Restrepo <73320777+ValentinaRestrepo2@users.noreply.github.com>
2023-04-14 21:02:47 -0500

    All the CRUD operations defined and implemented #2

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -1,9 +1,42 @@
-import express, {Request,Response} from "express"
+import express, { Request, Response, json } from 'express'
+const server = express();
 
-const server =express()
-server.get('/api', (req:Request, res:Response)=>{
-res.send({message:'Hello world'})
+server.use(json())
+
+const users: { [key: string  | number ]: any } = {}
+
+server.get('/api', (req: Request, res: Response) => {
+  res.send(users)
 })
-server.listen(80,()=>{
-console.log("Aplicación corriendo")
+
+server.get('/api/:id', (req: Request, res: Response) => {
+  res.send(users[req.params.id])
+})
+
+server.post('/api', (req: Request, res: Response) => {
+  const id = Math.ceil((Math.random() * 10000 ))
+  users[id] = {
+    id,
+    ...req.body
+  }
+  res.send(users[id])
+})
+
+server.patch('/api/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+  users[id] = {
+    id,
+    ...req.body
+  }
+  res.send(users[id])
+})
+
+server.delete('/api/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+  delete users[id]
+  res.send()
+})
+
+server.listen(80, () => {
+  console.log('App is up and running')
 })
